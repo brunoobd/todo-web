92dae6b4e3f84fa5ebcd7f4ad63bc59973c09559
Bruno Duarte <bruno.duarte314@gmail.com>
2025-03-04 22:43:13 -0300

    build: add styled-components

---
+++ src/theme/index.ts
@@ -0,0 +1,24 @@
+export default {
+  COLORS: {
+    GRAY_100: "#F2F2F2",
+    GRAY_200: "#D9D9D9",
+    GRAY_300: "#808080",
+    GRAY_400: "#333333",
+    GRAY_500: "#262626",
+    GRAY_600: "#1A1A1A",
+    GRAY_700: "#0D0D0D",
+
+    BLUE: "#4EA8DE",
+    BLUE_DARK: "#1E6F9F",
+
+    PURPLE: "#8284FA",
+    PURPLE_DARK: "#5E60CE",
+
+    DANGER: "#E25858",
+  },
+  FONT_SIZE: {
+    SMALL: 0.75,
+    MEDIUM: 0.875,
+    LARGE: 1,
+  },
+};
